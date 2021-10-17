37180292a6dc785019f93154aa5506058a27259d
christian@voegtle.org <christian@voegtle.org>
2021-10-17 15:14:50 +0200

    blink internal LED too

--- demo_code/Project_1_LED_Blinking.py
+++ demo_code/Project_1_LED_Blinking.py
@@ -1,12 +1,14 @@
 import time
 from machine import Pin
 
- 
-LED = Pin(0, Pin.OUT)
 
+LED = Pin(0, Pin.OUT)
+onboardLED = Pin(25, Pin.OUT)
+
+count = 0
 while True:
-    LED.value(0)
-    time.sleep(0.5)
-    LED.value(1)
-    time.sleep(0.5)
+    count += 1
+    LED.value(count % 2)
+    onboardLED.value(count % 2 - 1)
+    time.sleep(0.8)
 
