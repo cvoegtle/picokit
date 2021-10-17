0f118f3c59b5460258eb533c18f25109ded6e900
christian@voegtle.org <christian@voegtle.org>
2021-10-17 15:19:25 +0200

    change speed and direction

--- demo_code/Project_2_Water_flowing_LED.py
+++ demo_code/Project_2_Water_flowing_LED.py
@@ -1,21 +1,47 @@
 from machine import Pin
 from time import sleep
 
- 
-LED1 = Pin(0, Pin.OUT)
-LED2 = Pin(1, Pin.OUT)
-LED3 = Pin(2, Pin.OUT)
-LED4 = Pin(3, Pin.OUT)
+START_INTERVAL = 0.3
+MIN_INTERVAL = 0.06
+INTERVAL_DECREMENT = 0.02
+INTERVAL_CHANGE_TIME = START_INTERVAL*2
 
-interval = 0.2
 
-while True:
-    LED1.toggle()
-    sleep(interval)
-    LED2.toggle()
-    sleep(interval)
-    LED3.toggle()
-    sleep(interval)
-    LED4.toggle()
-    sleep(interval)
+def flash_leds():
+    onboard_led = Pin(25, Pin.OUT)
 
+    LEDs = [Pin(i, Pin.OUT) for i in range(4)]
+    clear(LEDs)
+
+    interval = START_INTERVAL
+    interval_time = 0
+    while True:
+        interval_time += interval
+        if interval_time >= INTERVAL_CHANGE_TIME:
+            interval_time = 0
+            interval -= INTERVAL_DECREMENT
+
+        if interval < MIN_INTERVAL:
+            interval = START_INTERVAL
+            LEDs = reverse_leds(LEDs)
+            print(LEDs)
+
+        print(f'Interval {interval}')
+        onboard_led.toggle()
+        for led in LEDs:
+            led.toggle()
+            sleep(interval)
+
+
+def reverse_leds(leds):
+    leds = leds[::-1]
+    clear(leds)
+    return leds
+
+
+def clear(leds):
+    for led in leds:
+        led.value(0)
+
+
+flash_leds()
