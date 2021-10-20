e0c276231ef8820c99bd435f3d6da0b1283e9965
christian@voegtle.org <christian@voegtle.org>
2021-10-20 15:16:30 +0200

    Anzeige nur wenn eine Bewegung erkannt wurde

--- demo_code/Project_5_Motion_detect_by_using_PIR_sensor.py
+++ demo_code/Project_5_Motion_detect_by_using_PIR_sensor.py
@@ -3,23 +3,16 @@ from machine import I2C
 from time import sleep, sleep_ms
 from machine_i2c_lcd import I2cLcd
 
-i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=400000)
+i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=400000)
 addr = i2c.scan()[0]
-# print(hex(addr[0]))
+print(addr)
 lcd = I2cLcd(i2c, addr, 2, 16)
 pir_sensor = Pin(15, Pin.IN)
 
 while True:
     reading = pir_sensor.value()
-    print(reading)
- 
     if reading == 1:
-        lcd.putstr("Motion Detect:\n")
-        lcd.putstr("Move Detected!")
-        sleep(1)
-        lcd.clear()
-    else:
-        lcd.putstr("Motion Detect:\n")
-        lcd.putstr("No Move Detect!")
+        lcd.putstr("Bewegung\n")
+        lcd.putstr("erkannt")
         sleep(1)
         lcd.clear()
