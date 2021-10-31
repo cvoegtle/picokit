913265e14c0e889088aa3a17c3b9071c84d5c865
christian@voegtle.org <christian@voegtle.org>
2021-10-31 20:22:43 +0100

    Projekt 8, ruhige Anzeige

--- demo_code/Project_8_8x8_MAX7219_matrix_display_CPU_Temperature.py
+++ demo_code/Project_8_8x8_MAX7219_matrix_display_CPU_Temperature.py
@@ -5,7 +5,7 @@ from utime import sleep
 
 MAX7219_NUM = 4
 MAX7219_INVERT = False
-MAX7219_SCROLL_DELAY = 0.05
+READ_TEMPERATURE_DELAY = 5
 cs_pin = 5
 
 spi = SPI(0)
@@ -13,16 +13,15 @@ display = max7219.Matrix8x8(spi=spi, cs=Pin(cs_pin), num=MAX7219_NUM)
 display.brightness(2)
 
 p = MAX7219_NUM * 8
-to_volts = 3.3 / 65535 
+to_volts = 3.3 / 65535
 temper_sensor = ADC(4)
 
 while True:
     temper_volts = temper_sensor.read_u16() * to_volts
     celsius_degrees = 27 - (temper_volts - 0.706) / 0.001721
-    text = 'Hello there, the Temperature of CPU is:' + str(round(celsius_degrees, 2)) + ' C'
+    text = str(round(celsius_degrees, 1)) + '°C'
 
-    for p in range(MAX7219_NUM * 8, len(text) * -8 - 1, -1):
-        display.fill(MAX7219_INVERT)
-        display.text(text, p, 1, not MAX7219_INVERT)
-        display.show()
-        sleep(MAX7219_SCROLL_DELAY)
+    display.fill(MAX7219_INVERT)
+    display.text(text, 0, 1, not MAX7219_INVERT)
+    display.show()
+    sleep(READ_TEMPERATURE_DELAY)
